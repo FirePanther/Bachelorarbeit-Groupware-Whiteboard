fd647c273a414f7f37b748954b75937f907452aa
FirePanther <git@suat.be>
2015-08-16 23:06:07 +0200

    Extended by hex color

--- client/js/Tools.class.js
+++ client/js/Tools.class.js
@@ -110,6 +110,8 @@ Tools.prototype.getColor = function(colorID) {
 		} else {
 			return this.colorIDs[this.options.color];
 		}
+	} else if (/^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(colorID)) {
+		return colorID;
 	} else {
 		return this.colorIDs[colorID];
 	}
